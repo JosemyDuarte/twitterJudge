06d1f894a38f3e2ad019f22934bceb3800ddc25a
Josemy Duarte <duartejosemy@gmail.com>
2016-07-10 13:36:00 -0400

    tests

--- tests/wordcount.py
+++ tests/wordcount.py
@@ -1,37 +1,29 @@
-import pytest
+""" wordcount example using the rdd api, we'll write a test for this """
+from __future__ import print_function
 
-import tools
+import sys
+from operator import add
 
-
-@pytest.fixture(scope="session")
-def spark_context(request):
-    """ fixture for creating a spark context
-    Args:
-        request: pytest.FixtureRequest object
-    """
-    conf = (SparkConf().setMaster("local[2]").setAppName("pytest-pyspark-local-testing"))
-    sc = SparkContext(conf=conf)
-    request.addfinalizer(lambda: sc.stop())
-
-    quiet_py4j()
-    return sc
+from pyspark import SparkContext
 
 
-pytestmark = pytest.mark.usefixtures("spark_context")
+def do_word_counts(lines):
+    """ count of words in an rdd of lines """
+
+    counts = (lines.flatMap(lambda x: x.split())
+              .map(lambda x: (x, 1))
+              .reduceByKey(add)
+              )
+    results = {word: count for word, count in counts.collect()}
+    return results
 
 
-def test_do_word_counts(spark_context):
-    """ test word couting
-    Args:
-        spark_context: test fixture SparkContext
-    """
-    test_input = [
-        ' hello spark ',
-        ' hello again spark spark'
-    ]
+if __name__ == "__main__":
 
-    input_rdd = spark_context.parallelize(test_input, 1)
-    results = wordcount.do_word_counts(input_rdd)
+    if len(sys.argv) != 2:
+        sys.exit("Usage: wordcount file}")
 
-    expected_results = {'hello': 2, 'spark': 3, 'again': 1}
-    assert results == expected_results
+    sc = SparkContext(appName="PythonWordCount")
+    lines = sc.textFile(sys.argv[1], 1)
+
+    print(do_word_counts(lines))
